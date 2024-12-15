fd7aa41f67a749e887dadfb8b723457c6b8390ee
Kirill Krymov <kkrymov.asc@gmail.com>
2024-12-15 13:55:32 +0300

    scroll reading text added 10px more to margin

--- app/components/PracticeExam/UI/ReadingText.jsx
+++ app/components/PracticeExam/UI/ReadingText.jsx
@@ -4,7 +4,7 @@ import QuizButton from "./QuizButton";
 
 export default function ReadingText({ content, title, onQuizButtonClick }) {
   return (
-    <div className="mt-10 mb-10 flex flex-col h-full overflow-hidden ">
+    <div className="mt-10 mb-20 flex flex-col h-full overflow-hidden ">
       <div className="flex-1 overflow-y-auto px-4 md:px-20 border-r border-gray-300 text-white bg-neutral-950 scrollbar-wider">
         <div className="py-10 mx-4">
           <div className="flex items-center justify-between mb-4 ">
